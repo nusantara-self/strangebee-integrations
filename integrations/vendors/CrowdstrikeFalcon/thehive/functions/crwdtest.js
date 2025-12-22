4c0d78a83100a5c07013d5a73c3885263e91e062
Fabien Bloume <15647296+nusantara-self@users.noreply.github.com>
2025-12-22 22:39:47 +0900

    Create crwdtest.js

---
+++ integrations/vendors/CrowdstrikeFalcon/thehive/functions/crwdtest.js
@@ -0,0 +1,12 @@
+/*---
+thehive:
+  name: CRWDTest
+  mode: Disabled
+  definition: crwdtest.js
+  description: Ingests CrowdstrikeFalcon Alerts, also processes observables & TTPs.
+  vendor: CrowdstrikeFalcon
+  kind: function
+  version: 1.0.0
+---*/
+
+// Test
